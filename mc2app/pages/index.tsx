22d30c0f129695a928abc5749f69dd76187a7fcc
Mimas <michal.maselko@awesome-ind.com>
2021-11-26 07:23:39 +0100

    first change to website

--- mc2app/pages/index.tsx
+++ mc2app/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">MC2 New website</a>
         </h1>
 
         <p className={styles.description}>
