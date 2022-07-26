12e1ecc58ffac3fd1a6b8723bc723589005ac806
Mimas <michal.maselko@awesome-ind.com>
2022-07-26 08:12:55 +0200

    m

--- mc2app/components/Members.tsx
+++ mc2app/components/Members.tsx
@@ -1,19 +1,44 @@
 /* eslint-disable @next/next/no-img-element */
+import { useEffect } from 'react'
 import styles from '../styles/Members.module.scss'
 
 const positionColors:any = {
-    hvac:"green",
-    ceo:"grey",
-    plumbing:"brown",
-    electric:"red",
-    office:"blue"
+    ceo:"#94989b",
+    hvac:"#008860",
+    plumbing:"#7e4a3f",
+    electric:"#e7342d",
+    office:"#017ebe"
+}
+
+const sortTest = (data:any[]) => {
+   data.map((item)=>{
+    switch (item.industry) {
+        case 'ceo':
+            return item.sort = 0
+        case 'hvac':
+            return item.sort = 1
+        case 'plumbing':
+            return item.sort = 2
+        case 'electric':
+            return item.sort = 3
+        case 'office':
+            return item.sort = 4
+        default:
+            break;
+        }
+   })
 }
 
 
 const Members = ({data}:{data:any}) => {
+    useEffect(()=>{
+        sortTest(data)
+        console.log('DATA',data)
+    },[data])
+
   return (
       <div className={styles.main}>
-            {data && data.sort((a:any,b:any)=> a.industry.localeCompare(b.industry)).map((item:any)=>{
+            {data && data.sort((a:any,b:any)=> a.sort - b.sort).map((item:any)=>{
                 return(
                     <div key={item._id} className={styles.profile}>
                         <div>
