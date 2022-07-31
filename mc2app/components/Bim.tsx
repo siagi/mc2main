fc326d7f8a090830413d4ef6bd4bbfc56a89e3b1
Mimas <michal.maselko@awesome-ind.com>
2022-07-31 10:27:11 +0200

    .

--- mc2app/components/Bim.tsx
+++ mc2app/components/Bim.tsx
@@ -7,12 +7,12 @@ const Bim:FunctionComponent = () => {
   return (
     <div className={styles.main}>
           <div className={styles.container_first}>
-              <div style={{fontWeight:700, color:'white',fontSize:'5rem', lineHeight:'3rem'}}>BIM <span style={{fontWeight:700, color:'#BDFF00',fontSize:'3rem'}}>REVIT</span></div>
+              <div className={styles.left_typo} >BIM <span className={styles.right_typo}>REVIT</span></div>
               <div className={styles.small_typo}>
                   <span style={{color:'#BDFF00'}}>check for Cooperation & </span>
                   <span style={{color:'white'}}>Oursourcing</span>
               </div>
-            <div style={{color:'white'}}>
+            <div className={styles.text}>
             From the small stuff to the big picture, organizes the work so teams know what to do, why it matters, and how to get it done.
             </div>
             <div className={styles.buttons_container}>
@@ -23,12 +23,12 @@ const Bim:FunctionComponent = () => {
 
 
       <div className={styles.container_second}>
-          <div style={{fontWeight:700, color:'white',fontSize:'5rem', lineHeight:'3rem'}}>Career in <span style={{fontWeight:700, color:'#BDFF00',fontSize:'3rem'}}>REVIT</span></div>
+          <div className={styles.left_typo}>Career <span className={styles.right_typo}>REVIT</span></div>
           <div className={styles.small_typo}>
               <span style={{color:'#BDFF00'}}>check for Cooperation & </span>
               <span style={{color:'white'}}>Oursourcing</span>
           </div>
-         <div style={{color:'white'}}>
+         <div className={styles.text}>
          We will traing to be great BIM specialist. In addition after trainign you will get the job in one of foreign markets like Aldi Billa or other...
          </div>
          <div className={styles.buttons_container}>
